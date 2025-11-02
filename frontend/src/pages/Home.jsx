51420bc7761669c70ba6f86c22b0e8cf8c4bb174
mohamed elsayed <mohamed25446066@gmail.com>
2025-11-02 16:11:45 +0200

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,17 +22,17 @@ import { motion } from 'framer-motion'
 import { fadeIn, } from './../components/common/motionFrameVarients';
 
 // background random images
-import backgroundImg1 from '../assets/Images/random bg img/coding bg1.jpg'
-import backgroundImg2 from '../assets/Images/random bg img/coding bg2.jpg'
-import backgroundImg3 from '../assets/Images/random bg img/coding bg3.jpg'
-import backgroundImg4 from '../assets/Images/random bg img/coding bg4.jpg'
-import backgroundImg5 from '../assets/Images/random bg img/coding bg5.jpg'
-import backgroundImg6 from '../assets/Images/random bg img/coding bg6.jpeg'
-import backgroundImg7 from '../assets/Images/random bg img/coding bg7.jpg'
-import backgroundImg8 from '../assets/Images/random bg img/coding bg8.jpeg'
-import backgroundImg9 from '../assets/Images/random bg img/coding bg9.jpg'
-import backgroundImg10 from '../assets/Images/random bg img/coding bg10.jpg'
-import backgroundImg111 from '../assets/Images/random bg img/coding bg11.jpg'
+import backgroundImg1 from '/assets/Images/random bg img/coding bg1.jpg'
+import backgroundImg2 from '/assets/Images/random bg img/coding bg2.jpg'
+import backgroundImg3 from '/assets/Images/random bg img/coding bg3.jpg'
+import backgroundImg4 from '/assets/Images/random bg img/coding bg4.jpg'
+import backgroundImg5 from '/assets/Images/random bg img/coding bg5.jpg'
+import backgroundImg6 from '/assets/Images/random bg img/coding bg6.jpeg'
+import backgroundImg7 from '/assets/Images/random bg img/coding bg7.jpg'
+import backgroundImg8 from '/assets/Images/random bg img/coding bg8.jpeg'
+import backgroundImg9 from '/assets/Images/random bg img/coding bg9.jpg'
+import backgroundImg10 from '/assets/Images/random bg img/coding bg10.jpg'
+import backgroundImg111 from '/assets/Images/random bg img/coding bg11.jpg'
 
 
 const randomImges = [
